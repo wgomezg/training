ad509927f1cf75a98a24d1d836492555f25edcf8
wgomezg <ing.wgomez@gmail.com>
2015-02-04 16:29:01 -0500

    count sites

--- siteCounter/mapper.py
+++ siteCounter/mapper.py
@@ -1,18 +1,47 @@
 #!/usr/bin/env python
 
 import sys
+from urlparse import urlparse
+
+#This algorithm is taking off 
+#http://stackoverflow.com/questions/1644362/best-way-to-parse-a-line-in-python-to-a-dictionary
+def lineDictionary(string):
+    inside_quotes = False
+    key = None
+    value = ""
+    dict = {}
+
+    for c in string:
+        if c == '"':
+            inside_quotes = not inside_quotes
+        elif c == '=' and not inside_quotes:
+            key = value
+            value = ''
+        elif c == ' ':
+            if inside_quotes:
+                value += ' ';
+            elif key and value:
+                dict[key] = value
+                key = None
+                value = ''
+        else:
+            value += c
+
+    dict[key] = value
+    return dict
+
+def extractSite(url):
+    parsed_uri = urlparse(url)
+    domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+    return domain
+
 
 # input comes from STDIN (standard input)
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
-    # split the line into words
-    words = line.split()
-    # increase counters
-    for word in words:
-        # write the results to STDOUT (standard output);
-        # what we output here will be the input for the
-        # Reduce step, i.e. the input for reducer.py
-        #
-        # tab-delimited; the trivial word count is 1
-        print '%s\t%s' % (word, 1)
+    # transform line into dictionary
+    dictionary = lineDictionary(line)
+    url = dictionary['url']
+    site = extractSite(url) 
+    print '%s\t%s' % (site, 1)

53e60f6cc09c9ea7078413ad1b453c516dcb9440
wgomezg <ing.wgomez@gmail.com>
2015-02-04 21:52:42 -0500

    ordenar resultado

--- siteCounter/reducer.py
+++ siteCounter/reducer.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
-from operator import itemgetter
+import operator
 import sys
 
-current_word = None
-current_count = 0
-word = None
+global dictionary
+dictionary = {}
 
 # input comes from STDIN
 for line in sys.stdin:
@@ -13,27 +12,11 @@ for line in sys.stdin:
     line = line.strip()
 
     # parse the input we got from mapper.py
-    word, count = line.split('\t', 1)
+    site, count = line.split('\t', 1)
+    
+    # store in the dictionary the values
+    dictionary[site] = int(count)
 
-    # convert count (currently a string) to int
-    try:
-        count = int(count)
-    except ValueError:
-        # count was not a number, so silently
-        # ignore/discard this line
-        continue
-
-    # this IF-switch only works because Hadoop sorts map output
-    # by key (here: word) before it is passed to the reducer
-    if current_word == word:
-        current_count += count
-    else:
-        if current_word:
-            # write result to STDOUT
-            print '%s\t%s' % (current_word, current_count)
-        current_count = count
-        current_word = word
-
-# do not forget to output the last word if needed!
-if current_word == word:
-    print '%s\t%s' % (current_word, current_count)
+#get the list of ordered keys
+for key, value in sorted(dictionary.iteritems(), key=lambda (k,v): (v,k), reverse=True):
+    print "%s: %s" % (key, value)
